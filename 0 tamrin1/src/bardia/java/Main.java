59911d55ea10a4374ad1964f5dac2a3a13bc40b0
Bardia <100039622+Bardia-Mi@users.noreply.github.com>
2022-03-12 23:33:29 +0330

    Add files via upload

---
+++ 0 tamrin1/src/bardia/java/Main.java
@@ -0,0 +1,16 @@
+//tamrin 1 bardia mirakhorloo 40012341054176
+package bardia.java;
+public class Main
+{
+    public static void main(String[] args) {
+        int x=4;
+        for (int i=1;i<=x;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
