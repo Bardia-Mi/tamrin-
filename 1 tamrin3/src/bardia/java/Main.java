7da03a5b167bbf8f9321e716f2f4fb449b090226
Bardia <100039622+Bardia-Mi@users.noreply.github.com>
2022-03-12 23:46:08 +0330

    Add files via upload

---
+++ 1 tamrin3/src/bardia/java/Main.java
@@ -0,0 +1,24 @@
+package bardia.java;
+import java.util.Scanner;
+public class Main
+{
+   public static void main(String[] args)
+   {
+      Scanner bardia=new Scanner(System.in);
+      Mohasebat t=new Mohasebat();
+      t.a();
+      t.a= bardia.nextDouble();
+      t.b();
+      t.b= bardia.nextDouble();
+      t.c();
+      t.c= bardia.nextDouble();
+      t.d();
+      t.d= bardia.nextDouble();
+      t.e();
+      t.e= bardia.nextDouble();
+      t.f();
+      t.f= bardia.nextDouble();
+      t.x();
+      t.y();
+   }
+}
---
+++ 1 tamrin3/src/bardia/java/Mohasebat.java
@@ -0,0 +1,40 @@
+package bardia.java;
+public class Mohasebat
+{
+    public double a,b,c,d,e,f,x,y;
+    public void a()
+    {
+        System.out.println("ENTER a:");
+    }
+    public void b()
+    {
+        System.out.println("ENTER b:");
+    }
+    public void c()
+    {
+        System.out.println("ENTER c:");
+    }
+    public void d()
+    {
+        System.out.println("ENTER d:");
+    }
+    public void e()
+    {
+        System.out.println("ENTER e:");
+    }
+    public void f()
+    {
+        System.out.println("ENTER f:");
+    }
+
+    public void x()
+    {
+        x=(((e*d)-(b*f))/((a*d)-(b*c)));
+        System.out.println("X is :"+x);
+    }
+    public void y()
+    {
+        y=(((a*f)-(e*c))/((a*d)-(b*c)));
+        System.out.println("Y is :"+y);
+    }
+}
